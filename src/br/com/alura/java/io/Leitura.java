ff73056bec0f1ec372b18d024a87b14a84b649a2
Gabriel <ga.mouraguedes@gmail.com>
2021-04-13 19:08:21 -0300

    Implementação de leitura
    Implementação simples de leitura de um arquivo txt

--- src/br/com/alura/java/io/Leitura.java
+++ src/br/com/alura/java/io/Leitura.java
@@ -1,7 +1,16 @@
 package br.com.alura.java.io;
 
+import java.io.*;
+
 public class Leitura {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream file = new FileInputStream("leitura.txt");
+        InputStreamReader reader = new InputStreamReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String linha = bufferedReader.readLine();
+        System.out.println(linha);
+        bufferedReader.close();
     }
 }
