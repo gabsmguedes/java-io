3ded4d0e4ceddbc0af0560c3326378417468456e
Gabriel <ga.mouraguedes@gmail.com>
2021-04-15 19:15:59 -0300

    Melhorando leitura de csv
    Setando locale para nao dar exception e melhorando a forma de mostrar os valores do csv

--- src/br/com/alura/java/io/LeituraCsv.java
+++ src/br/com/alura/java/io/LeituraCsv.java
@@ -1,6 +1,7 @@
 package br.com.alura.java.io;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class LeituraCsv {
@@ -11,6 +12,19 @@ public class LeituraCsv {
         while (scanner.hasNextLine()) {
             String nextLine = scanner.nextLine();
             System.out.println(nextLine);
+
+            Scanner linhaScanner = new Scanner(nextLine);
+            linhaScanner.useLocale(Locale.US);
+            linhaScanner.useDelimiter(",");
+            String tipoConta = linhaScanner.next();
+            int agencia = linhaScanner.nextInt();
+            int numero = linhaScanner.nextInt();
+            String titular = linhaScanner.next();
+            double saldo = linhaScanner.nextDouble();
+
+            System.out.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s:  %08.2f %n",
+                    tipoConta, agencia, numero, titular, saldo);
+            linhaScanner.close();
         }
         scanner.close();
     }
